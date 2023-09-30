93089549c010177384e2745a7222e7fa4d487d86
yamano <yamano@cb.com>
2023-09-30 19:00:53 +0000

    created layout.html andindex.html

--- Mclass/views.py
+++ Mclass/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def register(request):
+    pass
+
+def login(request):
+    pass
+
+def index(request):
+    return render(request, 'Mclass/index.html')
