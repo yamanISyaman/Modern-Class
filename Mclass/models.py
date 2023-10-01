b1e2fcb7fe840cbef3ad80ae01346ba16c6e9395
yamano <yamano@cb.com>
2023-10-01 13:26:39 +0000

    Implemented Register page and view

--- Mclass/models.py
+++ Mclass/models.py
@@ -3,4 +3,6 @@ from django.db import models
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    full_name = models.CharField(max_length=50)
+    is_teacher = models.BooleanField(default=False)
+    username = models.EmailField(unique=True)
--- Mclass/views.py
+++ Mclass/views.py
@@ -1,11 +1,54 @@
+import json
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+
+from .models import User
 
 # Create your views here.
 
 def register(request):
-    pass
+    if request.method == "POST":
+        full_name = request.POST["full-name"]
+        email = request.POST["email"]
+        role = request.POST["role"]
+        is_teacher = True if role == "teacher" else False
 
-def login(request):
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirm-password"]
+        if password != confirmation:
+            return render(request, "Mclass/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # make sure that fields are not left empty
+        if not password or not confirmation or not email or not full_name :
+            return render(request, "Mclass/register.html", {
+                "message": "All Fields Are Required."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(email.lower(), password=password, full_name=full_name, is_teacher=is_teacher)
+            user.save()
+        except IntegrityError:
+            return render(request, "Mclass/register.html", {
+                "message": "Email Already Has an Account"
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "Mclass/register.html")
+
+
+def login_view(request):
     pass
 
 def index(request):
